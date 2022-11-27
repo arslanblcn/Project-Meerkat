bcb678106bd977722a1a4b215e1213324b58c66e
hsinem <hsinembaskalgmail.com>
2022-11-27 18:07:14 +0300

    Subdomain table updated

--- frontend/meerkat/src/main.js
+++ frontend/meerkat/src/main.js
@@ -4,14 +4,21 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import VueApexCharts from 'vue-apexcharts'
+import mdiVue from 'mdi-vue/v2'
+import * as mdijs from '@mdi/js'
+
+Vue.use(mdiVue, {
+    icons: mdijs
+})
+
 Vue.use(VueApexCharts)
 
 Vue.component('apex-chart', VueApexCharts)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
