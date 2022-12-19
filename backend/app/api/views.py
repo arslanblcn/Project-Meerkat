e8ba7088c5cb1af323bf9ca275ca617ec75f2598
hsinem <hsinembaskalgmail.com>
2022-12-19 22:31:26 +0300

    login and register

--- backend/app/api/views.py
+++ backend/app/api/views.py
@@ -8,7 +8,27 @@ from datetime import datetime
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # Create your views here.
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
 
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
+# Register API
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+        "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        "token": AuthToken.objects.create(user)[1]
+        })
 
 class bypass403(APIView):
     def post(self, request, *args, **kwargs):
